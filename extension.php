37fdcf55aa3902583e62673dd2884c1c01cfa27d
eckynde <github@dunker-it.de>
2026-04-30 01:05:09 +0200

    Avoid caching the cache (for not explicitly specified thumbnails)

--- extension.php
+++ extension.php
@@ -85,9 +85,14 @@ final class ImageCacheExtension extends Minz_Extension
 	    return strcmp(substr(strtolower($url), 0, 5), "data:") != 0;
     }
 
+    public static function is_cache_url(string $url): bool
+    {
+        return str_starts_with($url, FreshRSS_Context::userConf()->image_cache_url);
+    }
+
     public static function send_proactive_cache_request(string $url): bool
     {
-        if (FreshRSS_Context::userConf()->image_cache_post_enabled && self::is_remote_url($url)) {
+        if (FreshRSS_Context::userConf()->image_cache_post_enabled && self::is_remote_url($url) && !self::is_cache_url($url)) {
             $post_url = FreshRSS_Context::userConf()->image_cache_post_url;
             return self::curlPostRequest($post_url, array("access_token" => FreshRSS_Context::userConf()->image_cache_access_token, "url" => $url));
         }
@@ -96,7 +101,7 @@ final class ImageCacheExtension extends Minz_Extension
 
     public static function getCacheImageUri(string $url): string
     {
-        if (!self::is_remote_url($url)) {
+        if (!self::is_remote_url($url) || self::is_cache_url($url)) {
             return $url;
         }
         $url = rawurlencode($url);
